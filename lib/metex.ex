453d1fa7e978740712a05acec2f6e51709b33ddd
GeoffreyPS <geoffreyps@gmail.com>
2016-01-26 22:29:29 -0600

    Remove worker functions from Metex GenServer

--- lib/metex.ex
+++ lib/metex.ex
@@ -1,18 +1,3 @@
 defmodule Metex do
 	use GenServer
-
-	## Client API
-
-	def start_link(opts \\ []) do
-		GenServer.start_link(__MODULE__, :ok, opts)
-	end
-
-	## Server Callbacks
-
-	def init(:ok) do
-		{:ok, %{}}
-	end
-
-	## Helper Functions
-
 end
